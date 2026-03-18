9bc4902accd114447ca44618906efadc6fc45c9e
sunpriser <jhj123465@163.com>
2026-03-18 15:01:09 +0800

    add the struct pointer(three)

--- 链表.cpp
+++ 链表.cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+//链表结构体
+/*
+    structure body content：Integer
+    define name: ILLST
+
+*/
+
 typedef struct I_Linked_List
 {
     int integer_node;
     struct I_Linked_List *next;
     /* data */
 }ILLST;
+
+//添加结构体指针
+ILLST *p, *head, *tail;
+
 
